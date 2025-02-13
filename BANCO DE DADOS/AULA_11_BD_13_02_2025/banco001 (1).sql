a8abd6b2943befa91dc9e7fd5d246b5db04ed0e9
Bill <williamvinicius007@gmail.com>
2025-02-13 11:58:06 -0300

    AULABD-13-02-25-

---
+++ BANCO DE DADOS/AULA_11_BD_13_02_2025/banco001 (1).sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 13, 2025 at 03:55 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banco001`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabela001`
+--
+
+CREATE TABLE `tabela001` (
+  `id_tabela001` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `sobrenome` varchar(30) NOT NULL,
+  `idade` int(3) NOT NULL,
+  `endereco` varchar(50) NOT NULL,
+  `numero` int(5) NOT NULL,
+  `complemento` varchar(10) DEFAULT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` char(2) NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `sexo` char(1) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `telefone` bigint(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `obs` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabela002`
+--
+
+CREATE TABLE `tabela002` (
+  `id_tabela001` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `sobrenome` varchar(30) NOT NULL,
+  `idade` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabela003`
+--
+
+CREATE TABLE `tabela003` (
+  `id_tabela003` int(11) NOT NULL,
+  `nomeProduto` varchar(50) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `peso` float(5,2) NOT NULL,
+  `preco` double(5,2) NOT NULL,
+  `dataEntrada` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tabela001`
+--
+ALTER TABLE `tabela001`
+  ADD PRIMARY KEY (`id_tabela001`);
+
+--
+-- Indexes for table `tabela002`
+--
+ALTER TABLE `tabela002`
+  ADD PRIMARY KEY (`id_tabela001`);
+
+--
+-- Indexes for table `tabela003`
+--
+ALTER TABLE `tabela003`
+  ADD PRIMARY KEY (`id_tabela003`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tabela001`
+--
+ALTER TABLE `tabela001`
+  MODIFY `id_tabela001` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tabela002`
+--
+ALTER TABLE `tabela002`
+  MODIFY `id_tabela001` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tabela003`
+--
+ALTER TABLE `tabela003`
+  MODIFY `id_tabela003` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ BANCO DE DADOS/AULA_11_BD_13_02_2025/pampauber.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 13, 2025 at 03:54 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pampauber`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mecanico`
+--
+
+CREATE TABLE `mecanico` (
+  `id_mecanico` int(11) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `celular` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `motorista`
+--
+
+CREATE TABLE `motorista` (
+  `id_motorista` int(11) NOT NULL,
+  `celular` bigint(11) NOT NULL,
+  `categoria` char(1) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `numero` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `servico`
+--
+
+CREATE TABLE `servico` (
+  `id_servico` int(11) NOT NULL,
+  `placa` varchar(30) NOT NULL,
+  `servico` varchar(30) NOT NULL,
+  `numero` int(10) NOT NULL,
+  `nomeVeiculo` varchar(50) NOT NULL,
+  `nomeMotorista` varchar(10) DEFAULT NULL,
+  `data_servico` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `veiculo`
+--
+
+CREATE TABLE `veiculo` (
+  `id_veiculo` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `marca` varchar(30) NOT NULL,
+  `placa` varchar(10) NOT NULL,
+  `modelo` varchar(30) NOT NULL,
+  `ano` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mecanico`
+--
+ALTER TABLE `mecanico`
+  ADD PRIMARY KEY (`id_mecanico`);
+
+--
+-- Indexes for table `motorista`
+--
+ALTER TABLE `motorista`
+  ADD PRIMARY KEY (`id_motorista`);
+
+--
+-- Indexes for table `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`id_servico`);
+
+--
+-- Indexes for table `veiculo`
+--
+ALTER TABLE `veiculo`
+  ADD PRIMARY KEY (`id_veiculo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mecanico`
+--
+ALTER TABLE `mecanico`
+  MODIFY `id_mecanico` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `motorista`
+--
+ALTER TABLE `motorista`
+  MODIFY `id_motorista` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `id_servico` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `veiculo`
+--
+ALTER TABLE `veiculo`
+  MODIFY `id_veiculo` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
