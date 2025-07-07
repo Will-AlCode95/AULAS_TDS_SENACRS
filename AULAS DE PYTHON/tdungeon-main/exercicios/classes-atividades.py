9b1ca711256951c20bd2d4c643d6d0c92cf9b45a
Will-Alcode95 <almeida.will95@gmail.com>
2025-07-06 23:49:32 -0300

    ATT

--- AULAS DE PYTHON/tdungeon-main/exercicios/classes-atividades.py
+++ AULAS DE PYTHON/tdungeon-main/exercicios/classes-atividades.py
@@ -15,12 +15,54 @@
 #5. **Classe com herança**  
 #   Implemente uma classe `PortaSecreta` que herda de `Porta` e adiciona um método `revelar()` que imprime "Uma porta secreta foi revelada!".
 
+#1
 class Porta():
     def __init__(self, aberta=bool):
-        self.aberta = aberta
+        self.aberta = False
     
     def abrir(self):
-        self.aberta(True)
+        self.aberta = True
 
-porta1 = Porta()
-print(porta1)
+#2
+class Chave():
+    def __init__(self, codigo=10):
+      self.codigo = codigo
+    
+print(Chave().codigo)
+
+#3
+class Tesouro():
+    def __init__(self, valor=100):
+        self.valor = valor
+    
+    def coletar(self):
+      print(f"O tesouro de valor {self.valor} foi Coletado!")
+
+caçador = Tesouro()
+caçador.coletar()
+
+#4. **Classe com atributos privados**  
+#   Defina uma classe `Cofre` com um atributo privado `_senha` e um método `verificar_senha(senha)` que retorna `True` se a senha estiver correta.
+
+
+
+
+class Cofre():
+    def __init__ (self, senha=5050):
+      self.__senha = str(senha)
+
+    def verif_senha(self, senha_digi):
+          return str(senha_digi) == self.__senha
+
+cofre = Cofre(5050)
+verif = input("digite a senha:")
+
+if cofre.verif_senha(verif):
+    print("Acesso Alowed")
+else:
+    print("wrong password")
+
+#5. **Classe com herança**  
+#   Implemente uma classe `PortaSecreta` que herda de `Porta` e adiciona um método `revelar()` que imprime "Uma porta secreta foi revelada!".
+
+
